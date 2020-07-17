b8803ec8a1ec9d08c3a4c168cb80269f1131d1ff
sarasorensen <51406325+sarasorensen@users.noreply.github.com>
2020-07-17 10:54:16 +0200

    Front page style finished

--- script.js
+++ script.js
@@ -1,86 +1,97 @@
-var char = [
+//scroll to characters
+let button = document.getElementById("button");
+
+button.addEventListener("click", function (event) {
+  document
+    .getElementById("card-row")
+    .scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+  event.stopPropagation();
+});
+
+const char = [
   {
     title: "Cersei Lannister",
-    aliases: "The Queen.",
-    skills: "Mind games, persuasive & control.",
+    aliases: "The Queen",
+    skills: "mind games, persuasiveness & control of kingdoms.",
     imageUrl: "images/characters/char-cer.png",
-    alt: "image of a rocket",
-    id: 1,
+    alt: "image of an illustrated character with long blonde hair",
+    id: "Cersei",
   },
   {
     title: "Arya Stark",
-    aliases: "Wolf girl or The Blind girl.",
-    skills: "Assassination skills, patient & martial arts.",
+    aliases: "Wolf girl",
+    skills: "assassination skills, patience, changing faces & martial arts.",
     imageUrl: "images/characters/char-ari.png",
-    alt: "image of a rocket",
-    id: 2,
+    alt: "image of an illustrated character with short hair",
+    id: "Arya",
   },
   {
     title: "Jamie Lannister",
-    aliases: "Kingslayer.",
-    skills: "Warfare, swords & charisma.",
+    aliases: "Kingslayer",
+    skills: "warfare, swords, money & charisma.",
     imageUrl: "images/characters/char-jam.png",
-    alt: "image of a rocket",
-    id: 3,
+    alt: "image of an illustrated character with blonde hair",
+    id: "Jamie",
   },
   {
     title: "Khal Drogo",
     aliases: "Dothraki Leader",
-    skills: "Most feared and strongest warrier of the Khals.",
+    skills: "most feared and strongest of the Khals, a good leader.",
     imageUrl: "images/characters/char-ka.png",
-    alt: "image of a rocket",
-    id: 4,
+    alt: "image of an illustrated character with a beard",
+    id: "Khal",
   },
   {
     title: "Jon Snow",
-    aliases: "King in the north.",
-    skills: "Warfare, swords & likeable.",
+    aliases: "King in the north",
+    skills: "warfare, swords, riding dragons & likeability",
     imageUrl: "images/characters/char-jon.png",
     alt: "image of an illustrated character with a mustache and black hair",
-    id: 5,
+    id: "Jon",
   },
   {
     title: "Melisandre",
-    aliases: "The Red Woman.",
-    skills: "Witchcraft & wisdom.",
+    aliases: "The Red Woman",
+    skills: "witchcraft, speaking to gods & wisdom.",
     imageUrl: "images/characters/char-meli.png",
-    alt: "image of a rocket",
-    id: 6,
+    alt: "image of an illustrated character with a red dress",
+    id: "Melisandre",
   },
   {
     title: "Grey Worm",
-    aliases: "Master of War.",
-    skills: "Commander from the Unsullied & Loyalty.",
+    aliases: "Master of War",
+    skills: "commander of the Unsullied, warrior & loyalty.",
     imageUrl: "images/characters/char-wor.png",
-    alt: "image of a rocket",
-    id: 7,
+    alt: "image of an illustrated character with soldier clothes.",
+    id: "Grey",
   },
   {
     title: "Sansa",
-    aliases: "Queen of the North.",
-    skills: "Mindgames & Survivor.",
+    aliases: "Queen of the North",
+    skills: "mind games, leading the North & Survivor.",
     imageUrl: "images/characters/char-san.png",
-    alt: "image of a rocket",
-    id: 8,
+    alt: "image of an illustrated character with long red hair.",
+    id: "Sansa",
   },
   {
     title: "Daenerys Targaryen",
-    aliases: "Mother of Dragons.",
-    skills: "Dragons & fire.",
+    aliases: "Mother of Dragons",
+    skills: "dragons, freeing slaves, warfare & fire.",
     imageUrl: "images/characters/char-dan.png",
-    alt: "image of a rocket",
-    id: 9,
+    alt: "image of an illustrated character with longe white hair",
+    id: "Daenerys",
   },
   {
     title: "Tyrion Lannister",
-    aliases: "The Imp or Halfman.",
-    skills: "Charisma & the hand of Daenerys.",
+    aliases: "The Imp",
+    skills: "charisma, knowledge & the hand of Daenerys Targaryen.",
     imageUrl: "images/characters/char-tyr.png",
-    alt: "image of a rocket",
-    id: 10,
+    alt: "image of an illustrated character with brown hair.",
+    id: "Tyrion",
   },
 ];
 
+//display characters
 var displayedChar = char;
 
 var container = document.querySelector("#cards");
@@ -92,13 +103,36 @@ function renderChar() {
 
   displayedChar.forEach(function (character) {
     container.innerHTML += `
-    <div class="card">
-        <h3>${character.title}</h3>
-        <img onclick="displayImage(this);" alt="${character.alt}" class="card-img"  src="${character.imageUrl}"</img>
-        <p>Also known as ${character.aliases}</p>
-        <p >Some known skills are: ${character.skills}</h2>
+    <div onClick="char_click(this.id)" id=${character.id} class="card">
+        <img   alt="${character.alt}" class="card-img"  src="${character.imageUrl}"</img>
+        <div class="card-box"> 
+
+        <div class="title-box">
+        <h3 class="title">${character.title}</h3>
+        <p class="alias"> ${character.aliases}</p>
+        </div>
+
+        <p class="description">This character is known for ${character.skills}</p>
+        <button type="button" onClick="char_click(this.id)">Select</button>
+        </div>
         </div>`;
   });
 }
 
 renderChar();
+
+//display box when char clicked
+const playersBox = document.getElementById("players-box");
+
+function char_click(clicked_id) {
+  playersBox.style.display = "block";
+
+  console.log(clicked_id);
+}
+
+//Close window if user clicks outside it
+window.onclick = function (event) {
+  if (event.target == playersBox) {
+    playersBox.style.display = "none";
+  }
+};
