d7769d3411e324ca556b752ccfa1df97b889a7ef
sarasorensen <51406325+sarasorensen@users.noreply.github.com>
2020-09-10 10:25:57 +0200

    Add files via upload

--- tokens.js
+++ tokens.js
@@ -4,6 +4,20 @@ const trapBox = document.getElementById("trapBox");
 let boardFieldArray = Array.from(boardField);
 let found;
 
+let colors = ["#234B52", "#a94442", "#FCD268", "#6EC8C7", "#2D5372", "#126d4f"];
+
+Array.prototype.getRandom = function (cut) {
+  var i = Math.floor(Math.random() * this.length);
+  if (cut && i in this) {
+    return this.splice(i, 1)[0];
+  }
+  return this[i];
+};
+
+for (let i = 0; i < boardFieldArray.length; i++) {
+  boardFieldArray[i].style.backgroundColor = colors.getRandom();
+}
+
 function moveTokens() {
   for (let i = 0; i < boardFieldArray.length; i++) {
     const number = i + 1;
