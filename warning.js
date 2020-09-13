18655a29beb4b0e04c3294caf629d533421041db
sarasorensen <51406325+sarasorensen@users.noreply.github.com>
2020-09-13 12:06:37 +0200

    Delete warning.js

--- warning.js
+++
@@ -1,6 +0,0 @@
-//warning for clicking new game
-function warning() {
-    var answer = confirm("If you click ok, you will loose your progress in this current game.");
-    if (answer) window.location = "index.html";
-    localStorage.clear();
-}
